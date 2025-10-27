cc05bd0001195df2bb5c5a4695ee4603f348880c
Dante0214 <rn_chic@naver.com>
2025-10-27 09:14:09 +0900

    feat: 결과 출력 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import { Console } from "@woowacourse/mission-utils";
 import { getCarNames, getTryCount } from "./utils/input.js";
+import { MESSAGES } from "./constants/constants.js";
+import RacingGame from "./RacingGame.js";
+import Car from "./Car.js";
+import { getWinners } from "./utils/racingResult.js";
 class App {
   async run() {
     try {
       const names = await getCarNames();
       const tryCount = await getTryCount();
+
+      const cars = names.map((name) => new Car(name));
+
+      Console.print(MESSAGES.RESULT_TITLE);
+      const game = new RacingGame(cars);
+      game.play(tryCount);
+
+      const winners = getWinners(game.getCars());
+
+      Console.print(`${MESSAGES.FINAL_WINNER}${winners.join(", ")}`);
     } catch (error) {
       Console.print(error.message);
       throw error;
---
+++ src/utils/racingResult.js
@@ -0,0 +1,6 @@
+export function getWinners(cars) {
+  const maxPosition = Math.max(...cars.map((car) => car.getPosition()));
+  return cars
+    .filter((car) => car.getPosition() === maxPosition)
+    .map((car) => car.getName());
+}
